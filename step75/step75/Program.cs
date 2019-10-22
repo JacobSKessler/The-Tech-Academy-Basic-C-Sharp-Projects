a974e5dbfecefaf726516293e82969023829de86
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-21 19:32:25 -0700

    step 75

---
+++ step75/step75/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step75
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            short age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            Boolean dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            short tickets = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+
+            Boolean qualified = Convert.ToString( age > 15 && dui == 0 && tickets < 3);
+
+            Console.WriteLine(qualified);
+
+        }
+    }
+}
