d8cbf5254dfb57d0e66155674aa9d26e1936e6be
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-24 14:26:20 -0700

    step 106

---
+++ step106/step106/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 2,34,55,22,13,23 };
+
+
+
+            try
+            {
+                for(int i = 0; i < nums.Count; i++)
+                {
+                    Console.WriteLine("provide a whole number for division");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(nums[i] / input);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("please don't divide by zero");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("please enter an intiger");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("feel free to run the program again");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
