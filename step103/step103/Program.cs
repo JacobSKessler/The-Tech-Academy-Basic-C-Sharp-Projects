496dee214c5022913d968d397c9f7f1fad80b72f
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-24 13:53:27 -0700

    step103

---
+++ step103/step103/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 5 words one after another");
+            string[] words = new string[4];
+            for (int i = 0; i < 5; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+               Console.WriteLine(words[i]);
+            }
+            //fix was adding in i<=20 rather than i==i
+            for (int i = 0; i <= 20; i++) 
+            {
+                Console.WriteLine(Convert.ToString(i));
+            }
+
+            List<string> strList = new List<string>();
+            strList.Add("one");
+            strList.Add("two");
+            strList.Add("three");
+
+            bool isThere = false;
+
+            while (isThere == false)
+            {
+                Console.WriteLine("search for one two or three");
+                string input = Console.ReadLine();
+
+                for (int i; i < strList.Count; i++)
+                {
+                    if (strList[i] == input)
+                    {
+                        isThere = true;
+                        string index = Convert.ToString(i);
+                        Console.WriteLine(input + " is in index " + index);
+                    }
+                }
+                if (isThere == false)
+                {
+                    Console.WriteLine("input was bad");
+                }
+            }
+
+            isThere = false;
+            strList.Add("one");
+            strList.Add("two");
+            while (isThere == false)
+            {
+                Console.WriteLine("search for one two or three");
+                string input = Console.ReadLine();
+
+                for (int i; i < strList.Count; i++)
+                {
+                    if (strList[i] == input)
+                    {
+                        isThere = true;
+                        string index = Convert.ToString(i);
+                        Console.WriteLine(input + " is in index " + index);
+                    }
+                }
+                if (isThere == false)
+                {
+                    Console.WriteLine("input was bad");
+                }
+            }
+
+            List<string> strings = new List<string>();
+            bool dupe = false;
+            strings.Add("hellow world");
+            strings.Add("hellow world");
+
+            foreach (string str in strings)
+            {
+                if (dupe == false)
+                {
+                    Console.WriteLine(str);
+                    Console.WriteLine("first time on list");
+                    dupe = true;
+
+                }
+                else
+                {
+                    Console.WriteLine(str);
+                    Console.WriteLine("not firtst time on list");
+
+                }
+
+
+
+            }
+        }
+    }
+}
