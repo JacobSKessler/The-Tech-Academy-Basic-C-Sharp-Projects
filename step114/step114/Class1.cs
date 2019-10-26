b00e18de44fcaeffdc7ae64c5ee586c9e03e16c7
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-26 12:49:26 -0700

    step 114

---
+++ step114/step114/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step114
+{
+    class Class1
+    {
+        public static int Change(string x)
+        {
+            return Convert.ToInt32(x) + 5;
+        }
+
+        public static int Change(decimal x)
+        {
+            return Convert.ToInt32( x / 2);
+        }
+
+        public static int Change(int x)
+        {
+            return x * 2;
+        }
+
+    }
+}
---
+++ step114/step114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Change(12));
+            Console.WriteLine(Class1.Change(12.2m));
+            Console.WriteLine(Class1.Change("12"));
+            Console.ReadLine();
+
+        }
+    }
+}
