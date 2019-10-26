08a4d89f065ea1a2a1b92adcd8d42dc1989866c2
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-26 12:37:19 -0700

    step 113

---
+++ step113/step113/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    class Class1
+    {
+        public static int Combo(int x, int y)
+        {
+            x = x + 5;
+            Console.WriteLine(y);
+            return x;
+        }
+    }
+}
---
+++ step113/step113/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Class1.Combo(x:4, y:5);
+        }
+    }
+}
