d568eabba15c5544e7debbfa9a48a180a5c5b69c
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-26 12:12:16 -0700

    step 111

---
+++ step111/step111/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step111
+{
+    class Class1
+    {
+        public static int AddFive(int num)
+        {
+            int result = num + 5;
+            return result;
+        }
+        public static int SubFive(int num)
+        {
+            int result = num - 5;
+            return result;
+        }
+        public static int TimesFive(int num)
+        {
+            int result = num * 5;
+            return result;
+        }
+    }
+}
---
+++ step111/step111/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What whole number do you want to do math with?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Class1.AddFive(input));
+            Console.WriteLine(Class1.SubFive(input));
+            Console.WriteLine(Class1.TimesFive(input));
+            Console.ReadLine();
+        }
+    }
+}
