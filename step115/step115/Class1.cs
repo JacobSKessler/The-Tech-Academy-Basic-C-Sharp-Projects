015a6b642c567ad49444bcc104256d50301089d3
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-26 17:14:14 -0700

    step 115

---
+++ step115/step115/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step115
+{
+    class Class1
+    {
+        public static int TimesFive(int x, int y = 1)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                x = x * 5;
+            }
+            return x;
+        }
+
+    }
+}
---
+++ step115/step115/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number, then if you want enter a second whole number or just enter nothing");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2;
+            string inputString = Console.ReadLine();
+
+            //checks in second input is an int value and if it is assigns it to input2
+            int.TryParse(inputString, out input2);
+
+            int result = Class1.TimesFive(input1, input2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
