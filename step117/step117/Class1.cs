40459d07c5757bd0c05dff89bbc5a7d9fbeb8826
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-29 09:58:58 -0700

    step117

---
+++ step117/step117/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step117
+{
+    public static class Class1
+    {
+        public static void DivTwo(out int x, int input)
+        {
+            
+            int temp = input / 2;
+            x = temp;
+        }
+
+        public static void DivTwo()
+        {
+            int temp = 5;
+            
+        }
+    }
+}
+
---
+++ step117/step117/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            Console.WriteLine("Enter a whole number to get devided by 2");
+            Class1.DivTwo(out x, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("the new number is " + x);
+            Console.ReadLine();
+        }
+    }
+}
