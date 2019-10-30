e3b784e5559edfbaaa6def98bb68ecd2dfee2bfd
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-30 11:51:28 -0700

    step 124

---
+++ step124/step124/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee { FirstName = "Sample", LastName = "Student", id = 0001 };
+            example.SayName();
+            Console.ReadLine();
+        }
+    }
+}
