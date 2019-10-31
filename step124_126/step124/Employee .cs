59c3c4c2ddba062cb90c0f7bb0d8149a50720ff4
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-31 10:02:01 -0700

    update to include 129

--- step124_126/step124/Employee .cs
+++ step124_126/step124/Employee .cs
@@ -8,6 +8,20 @@ namespace step124
 {
     class Employee : Person, IQuittable
     {
+        public int id { get; set; }
+
+        public static bool operator !=(Employee x, Employee y)
+        {
+            bool diff = x.id != y.id;
+            return diff;
+        }
+        public static bool operator ==(Employee x, Employee y)
+        
+        {
+            bool same = x.id == y.id;
+            return same;
+        }
+
         public void SayName2()
         {
             SayName();
