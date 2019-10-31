93d28432b85c83afb4ac5cd5af70250ab7b59908
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-10-31 13:26:08 -0700

    Step 137

---
+++ step137/step137/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step137
+{
+    public struct Number
+    {
+        public Decimal Amount { get; set; }
+        
+
+        
+    }
+}
---
+++ step137/step137/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number { Amount = 2.222M };
+
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
