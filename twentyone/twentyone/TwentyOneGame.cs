c83e1910c8f5ed5f44cdd7f5bae904adbc6350c8
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-03 18:27:48 -0800

    update 21game

---
+++ twentyone/twentyone/TwentyOneGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyone
+{
+    public class TwentyOneGame : Game, IWalkAway
+    {
+        public TwentyOneDealer Dealer { get; set; }
+        public override void Play()
+        {
+            Dealer = new TwentyOneDealer();
+            foreach (Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            Console.WriteLine("Place your bet!");
+
+            foreach (Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
