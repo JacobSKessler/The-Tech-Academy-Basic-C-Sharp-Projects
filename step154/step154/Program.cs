d7e1472cb665bed75c737f047227ecd5fdb1f591
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-05 12:27:15 -0800

    step154

---
+++ step154/step154/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for number
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            //log number
+            using (StreamWriter file = new StreamWriter(@"C:\Users\kessl\OneDrive\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\logs\154log.txt", true))
+            {
+                file.WriteLine(input);
+            }
+            Console.WriteLine("\n" + "Whole log reads:");
+            //print back log
+            string[] lines = File.ReadAllLines(@"C:\Users\kessl\OneDrive\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\logs\154log.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.Read();
+        }
+    }
+}
