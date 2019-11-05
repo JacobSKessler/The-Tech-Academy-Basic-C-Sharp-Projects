c4e5642cfea546bada35bb1babde1a1bc3031dab
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-05 15:25:34 -0800

    step162

---
+++ step162/step162/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step162
+{
+    class Class1
+    {
+        private int id;
+        private string name;
+
+        public void Tag() : this(0, "") { }
+        public void Tag(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ step162/step162/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x = 5;
+            var y = "65";
+        }
+        //Constructors are in Class1
+        
+    }
+        
+    
+}
