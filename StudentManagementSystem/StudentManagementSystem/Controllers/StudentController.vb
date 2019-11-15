60043bcba9d57f19cf6b5f61fd5f8a95dccd6edb
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-15 13:12:57 -0800

    Student Manager project

---
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class StudentController : Controller
+
+        Private String _connectionString = @"Data Source=DESKTOP-5K61KI0\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+        ' GET: Student
+        Function Index() As ActionResult
+
+            Return View()
+        End Function
+    End Class
+End Namespace
