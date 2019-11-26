4985890677447e3e368d80bc4ed97c973d30c4e2
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-26 11:20:32 -0800

    update 75

--- step75/step75/Program.cs
+++ step75/step75/Program.cs
@@ -12,13 +12,13 @@ namespace step75
         {
             Console.WriteLine("What is your age?");
             short age = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("Have you ever had a DUI true or false?");
             Boolean dui = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("How many speeding tickets do you have?");
             short tickets = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Qualified?");
 
-            Boolean qualified = Convert.ToString( age > 15 && dui == 0 && tickets < 3);
+            Boolean qualified = ( age > 15 && dui == false && tickets < 3);
 
             Console.WriteLine(qualified);
 
