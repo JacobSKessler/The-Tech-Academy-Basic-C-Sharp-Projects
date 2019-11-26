7cfd324bd28412693641f012746d1b5a79231420
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-11-26 08:04:55 -0800

    update 162

--- step162/step162/Class1.cs
+++ step162/step162/Class1.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 namespace step162
 {
     class Class1
-    {
+    {   //when I did the method() : this(stuff) style like used in almost every place I could find including the LMS videos the : was listing as an error I can not figure out why for the life of me
         private int id;
-        private string name;
 
-        public void Tag() : this(0, "") { }
-        public void Tag(int id, string name)
+        public void Tag() 
+        {
+            Tag(10);
+        }
+
+        public void Tag(int id)
         {
             this.id = id;
-            this.name = name;
         }
+
+
+
     }
 }
